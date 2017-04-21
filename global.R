c2f287cf8dc3088220483d14389fb9129651056a
Tokhir Dadaev <zx8754@gmail.com>
2017-04-21 22:04:04 +0100

    clean up dev

--- global.R
+++ global.R
@@ -9,7 +9,7 @@
 #                    "googleVis"))
 
 library(shiny)
-library(shinyjs)
+#library(shinyjs)
 library(dtplyr)
 library(data.table)
 library(dplyr) 
@@ -25,9 +25,6 @@ library(lattice) # ggbio
 library(acepack) # ggbio
 library(cluster)
 library(DBI) # gene plot
-#library(networkD3) # graph plot
-#library(scales) #sankey
-#library(googleVis) #sankey
 
 #Bioconductor packages
 #source("http://bioconductor.org/biocLite.R")
@@ -40,23 +37,11 @@ library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 library(org.Hs.eg.db) # gene symobols
 library(rtracklayer) # bigwig
 
-#GitHub packages
-# install.packages("devtools")
-# devtools::install_github("gastonstat/arcdiagram")
-# library(arcdiagram) # arc plot
-
 # increase upload limit to 20Mb
 options(shiny.maxRequestSize = 20 * 1024 ^ 2)
 
-#Map fonts to Windows
-if(Sys.info()['sysname'] == "Windows") {
-  windowsFonts(Courier = windowsFont("TT Courier New"))
-  setInternet2(use = NA)
-  }
-
 #Custom functions
 source("Source/UDF.R", local = TRUE)
-
 
 # Data --------------------------------------------------------------------
 # prostate data region names
@@ -83,12 +68,3 @@ wgEncodeBroadHistoneFileDesc <-
   read.csv("Data/wgEncodeBroadHistone/wgEncodeBroadHistone.csv",
            stringsAsFactors = FALSE)
 
-
-# TESTING -----------------------------------------------------------------
-
-# TEMP hack!!!!
-# TxDb.Hsapiens.UCSC.hg19.knownGene <- 
-#   makeTxDbFromUCSC(genome = "hg19", tablename = "refGene")
-# 
-# saveDb(TxDb.Hsapiens.UCSC.hg19.knownGene, file="refGene.sqlite")
-#TxDb.Hsapiens.UCSC.hg19.knownGene <- loadDb("refGene.sqlite")
